a0b1019c262d933c3c1def2fed596eecfc25f405
Nehuel <nehuel2005@gmail.com>
2024-09-01 00:30:20 -0300

    test

--- PROGRAMACION_3/ejercicios/clase3/cl3ej2.py
+++ PROGRAMACION_3/ejercicios/clase3/cl3ej2.py
@@ -5,21 +5,18 @@
 # pero usted no conoce este valor (ni cuenta, por supuesto, con un método que se lo devuelva) y que la 
 # implementación del arreglo en su lenguaje de programación devuelve el mensaje de error ∞ siempre que se trata de acceder a un elemento A[i] con i > n.
 # Ayuda: puede buscar inspiración en la estrategia del ejercicio anterior.
-
 def generar_lista_infinita(A: list):
     for i in range(10000):
         A.append("∞")
 
 def encontrar_entero(A: list, x: int):
     def definir_rango(A: list, i: int) -> int:
-        if A[i + 1] == "∞":
+        if A[i] == "∞":
+            return definir_rango(A, i - 1)
+        elif A[i + 1] == "∞":
             return i
         else:
-            if A[i] == "∞":
-                definir_rango(A, i - 1)
-
-            else:
-                return definir_rango(A, i*2)
+            return definir_rango(A, i*2)
 
     def encontrar_entero_r(A: list, x: int, ini: int, fin: int):
         if ini == fin:
@@ -41,9 +38,7 @@ def encontrar_entero(A: list, x: int):
     return encontrar_entero_r(A, x, 0, fin)
 
 
-
-
-lista = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+lista = [1]
 generar_lista_infinita(lista)
-print(encontrar_entero(lista, 0))
+print(f"El numero 1 se encuetra en la posicion {encontrar_entero(lista, 1)}")
 
